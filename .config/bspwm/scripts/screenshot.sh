6cd04a3adc804d903e831be6ccd3f037a98494ec
mboufous <bfsmhmd@gmail.com>
2022-03-24 10:28:34 +0100

    Add someupdates - I forgot what was this about

--- .config/bspwm/scripts/screenshot.sh
+++ .config/bspwm/scripts/screenshot.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # options to be displayed
-option0="screen"
-option1="area"
-option2="window"
+option0="area2clipboard"
+option1="screen"
+option2="area"
+option3="window"
 
 # options to be displyed
-options="$option0\n$option1\n$option2"
+options="$option0\n$option1\n$option2\n$option3"
 
 selected="$(echo -e "$options" | rofi -no-config -no-lazy-grab -dmenu -theme ~/.config/rofi/launcher.rasi -p " ")"
 case $selected in
     $option0)
-        cd ~/Pictures/ && sleep 1 && scrot;;
+        scrot -s -e 'xclip -selection clipboard -t image/png -i $f';;
     $option1)
-        cd ~/Pictures/ && scrot -s;;
+        cd ~/Pictures/ && sleep 1 && scrot;;
     $option2)
+        cd ~/Pictures/ && scrot -s;;
+    $option3)
         cd ~/Pictures/ && sleep 1 && scrot -u;;
 esac
 
--- .config/polybar/scripts/env.sh
+++ .config/polybar/scripts/env.sh
@@ -1,2 +1,2 @@
 export REDSHIFT=on
-export REDSHIFT_TEMP=5900
+export REDSHIFT_TEMP=5300
